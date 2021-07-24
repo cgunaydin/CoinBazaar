ec7fcfa8a71f2876177b8541b1cda8e975900ad6
cagri.gunaydin <cagri.gunaydin@d-teknoloji.com.tr>
2021-07-24 14:58:42 +0300

    initinal commit

---
+++ CoinBazaar.Transfer.API/Controllers/TransferController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using CoinBazaar.Transfer.API.Requests;
+using CoinBazaar.Transfer.Application.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CoinBazaar.Transfer.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TransferController : ControllerBase
+    {
+        private IMediator _mediator { get; set; }
+        private IMapper _mapper { get; set; }
+        public TransferController(IMediator mediator, IMapper mapper)
+        {
+            _mediator = mediator;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTransferAsync(CreateTransferRequest request)
+        {
+            var domainResponse = await _mediator.Send(_mapper.Map<CreateTransferCommand>(request));
+
+            return Ok(domainResponse);
+        }
+    }
+}
